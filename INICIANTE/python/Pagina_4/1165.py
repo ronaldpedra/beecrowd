58b989324a9276bd835c1e0696fef286b1dc0d15
Ronald Pedra <ronaldpedra@gmail.com>
2024-06-06 12:52:53 -0300

    Update 1165.py

--- INICIANTE/python/Pagina_4/1165.py
+++ INICIANTE/python/Pagina_4/1165.py
@@ -1,13 +1,39 @@
 # uai
-n = int(input())
+#        1    2  3   4   5   6   7   8   9  10  soma algarismos
+# 1           2  3       5       7              impar
+# 2     11      13              17      19      par
+# 3             23                      29      impar
+# 4     31                      37              par
+# 5     41      43              47              impar
 
-if int(n/10) % 2 == 0:
-    print('par')
+
+def testar(n, primos_elementares):
+    '''aqui vai o docstring'''
+    ehprimo = 'é primo'
+    naoehprimo = 'não é primo'
+
+    if n in primos_elementares:
+        return ehprimo
+
+    if primos_elementares[-1] < n:
+        print('entrou')
+        baliza = primos_elementares[-1] + 1
+        continua = True
+        while continua:
+            if str(baliza)[-1] in ['1', '3', '7', '9']:
+                for primo in primos_elementares:
+                    if baliza % primo != 0:
+                        primos_elementares.append(baliza)
+                        continua = False
+                        testar(n, primos_elementares)
+                    else:
+                        return naoehprimo
+    return naoehprimo
+
+numero = int(input())
+
+primo_elementar = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+if str(numero)[-1] in ['1', '3', '7', '9']:
+    print(testar(numero, primo_elementar))
 else:
-    caso = str(n)
-    soma = 0
-    for i in range(len(caso)):
-        soma += int(caso[i])
-    print(soma)
-
-
+    print('não é primo')
