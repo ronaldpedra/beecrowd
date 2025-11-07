1045134cfe9766d5c358582a816ee10d64f38915
Ronald Pedra <69393789+ronaldpedra@users.noreply.github.com>
2025-11-07 11:47:03 -0300

    Create

---
+++ INICIANTE/python/Pagina_10/2756.py
@@ -0,0 +1,48 @@
+"""
+beecrowd | 2756
+Saída 10
+Por Roberto A. Costa Jr, UNIFEI BR Brazil
+
+Timelimit: 1
+O seu professor de programação gostaria que você fizesse um programa com 
+as seguintes características:
+
+Coloque sete espaços em branco e coloque o carácter 'A';
+Coloque seis espaços em branco e coloque o carácter 'B', um espaço em 
+branco e o carácter 'B';
+Coloque cinco espaços em branco e coloque o carácter 'C', três espaço em 
+branco e o carácter 'C';
+Coloque quatro espaços em branco e coloque o carácter 'D', cinco espaço 
+em branco e o carácter 'D';
+Coloque três espaços em branco e coloque o carácter 'E', sete espaço em 
+branco e o carácter 'E';
+Repita o procedimento 4;
+Repita o procedimento 3;
+Repita o procedimento 2;
+Repita o procedimento 1.
+Entrada
+Não há.
+
+Saída
+O resultado de seu programa deve ser escrito conforme o exemplo de saída.
+
+Exemplo de Entrada	Exemplo de Saída
+       A
+      B B
+     C   C
+    D     D
+   E       E	
+    D     D
+     C   C
+      B B
+       A	
+"""
+print('       A')
+print('      B B')
+print('     C   C')
+print('    D     D')
+print('   E       E')
+print('    D     D')
+print('     C   C')
+print('      B B')
+print('       A')
