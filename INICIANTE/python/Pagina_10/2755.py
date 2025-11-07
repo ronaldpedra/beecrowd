5b2f49f6fd405abf96c73f54ad57f72dce762d83
Ronald Pedra <69393789+ronaldpedra@users.noreply.github.com>
2025-11-07 11:33:27 -0300

    Create 2755.py

---
+++ INICIANTE/python/Pagina_10/2755.py
@@ -0,0 +1,30 @@
+"""
+beecrowd | 2755
+Saída 9
+Por Roberto A. Costa Jr, UNIFEI BR Brazil
+
+Timelimit: 1
+O seu professor de programação gostaria que você fizesse um programa com 
+as seguintes características:
+
+Mostre a seguinte frase na tela: "Ro'b'er to\/" (Entre o r e o t tem uma 
+tabulação);
+Mostre a seguinte frase na tela: (._.) ( l: ) ( .-. ) ( :l ) (._.);
+Mostre a seguinte frase na tela: (^_-)(-_-) (-_^);
+Mostre a seguinte frase na tela: ("_") ('.');
+Entrada
+Não há.
+
+Saída
+O resultado de seu programa deve ser escrito conforme o exemplo de saída.
+
+Exemplo de Entrada	Exemplo de Saída
+"Ro'b'er        to\\/"
+(._.) ( l: ) ( .-. ) ( :l ) (._.)
+(^_-) (-_-) (-_^)
+("_") ('.')
+"""
+print("\"Ro'b'er\tto\\/\"")
+print(r'''(._.) ( l: ) ( .-. ) ( :l ) (._.)''')
+print(r'''(^_-) (-_-) (-_^)''')
+print(r'''("_") ('.')''')
